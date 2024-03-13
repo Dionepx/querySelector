8f99fb186fa878d15b0b22fa5a48732572d1e28b
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-03-13 05:14:04 -0300

    Add files via upload

---
+++ querySelector/script.js
@@ -0,0 +1,9 @@
+// const paragraph = document.querySelector('p') // Vai pegar so o primeira p
+// const paragraph = document.querySelectorAll('p') // Vai pegar todos os p
+
+// const paragraph = document.querySelector('.error') // Vai pegar so a primeira mensagem de erro
+// const paragraph = document.querySelectorAll('.error') // Agora vai pegar as duas
+
+// const errorDiv = document.querySelector('div.error') // Agora vai pegar as duas
+
+// const h1 = document.querySelector('body > h1') // retoirna o primeiro elemento h1 que seja filho do elemento body
